2962db5f10e5c878358f43c6ae2942ed39630fa3
Đinh Quốc Cường <tr6r20@gmail.com>
2025-07-25 17:13:58 +0700

    ADD clear cache

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 // Load các file logic chia nhỏ trong inc/
 require get_template_directory() . '/inc/function-js.php';
 require get_template_directory() . '/inc/ajax.php';
@@ -7,3 +11,6 @@ require get_template_directory() . '/inc/enqueue.php';
 // Bạn có thể thêm các file khác ở đây:
 # require get_template_directory() . '/inc/menus.php';
 # require get_template_directory() . '/inc/theme-support.php';
+// Chặn cache trình duyệt & server
+
+?>
