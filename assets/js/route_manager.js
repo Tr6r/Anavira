3818c8ebe3ea103d2bdd56b7e4b0b0fc9b72c7ad
Đinh Quốc Cường <tr6r20@gmail.com>
2025-07-30 20:01:06 +0700

    CHANGE disable changeToHomePage()

--- assets/js/route_manager.js
+++ assets/js/route_manager.js
@@ -33,9 +33,9 @@ function RouteNavigation() {
 changeToHomePage();
 function changeToHomePage() {
     const btn_header = window.innerWidth < 450 ? document.getElementById('mobileHeader') : document.getElementById('desktopHeader');
-    if (!btn_header) return
-    btn_header.addEventListener('click', () => {
-        window.location.href = '/';
-    });
+    // if (!btn_header) return
+    // btn_header.addEventListener('click', () => {
+    //     window.location.href = '/';
+    // });
 
 }
