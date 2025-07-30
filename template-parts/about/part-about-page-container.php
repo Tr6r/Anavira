bf34395b4607b80af4dbaca2e9f80cbb5b9946ae
Đinh Quốc Cường <tr6r20@gmail.com>
2025-07-30 18:00:06 +0700

    UPDATE about page content

--- template-parts/about/part-about-page-container.php
+++ template-parts/about/part-about-page-container.php
@@ -2,7 +2,7 @@
     <div class="About_Container_Items">
         <div class="About_Container_Items_Tilte">
             <div class="About_Container_Items_Tilte_Icon">
-                <?php echo file_get_contents(get_template_directory() . '/assets/images/willow.svg'); ?>
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
             </div>
             <h2 class='About_Container_Items_Tilte_Text'>Một bình minh, một quyết định</h2>
         </div>
@@ -24,7 +24,7 @@
     <div class="About_Container_Items">
         <div class="About_Container_Items_Tilte">
             <div class="About_Container_Items_Tilte_Icon">
-                <?php echo file_get_contents(get_template_directory() . '/assets/images/willow.svg'); ?>
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
             </div>
             <h2 class='About_Container_Items_Tilte_Text'>Anavira: Thức tỉnh – Mạnh mẽ – Tái sinh</h2>
         </div>
@@ -39,10 +39,10 @@
         </div>
 
     </div>
-    <div class="About_Container_Items">
+    <div class="About_Container_Items padding-bottom pos_relative">
         <div class="About_Container_Items_Tilte">
             <div class="About_Container_Items_Tilte_Icon">
-                <?php echo file_get_contents(get_template_directory() . '/assets/images/willow.svg'); ?>
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
             </div>
             <h2 class='About_Container_Items_Tilte_Text'>Chúng tôi làm gì?</h2>
         </div>
@@ -79,4 +79,253 @@
         </div>
 
     </div>
+    <div class="About_Container_Items About_Products pos_relative ">
+        <div class="About_Container_Items_Contents Text_MarginLeft">
+            <p class="About_Container_Items_Content ">
+                Các dòng sản phẩm
+            </p>
+        </div>
+
+        <div class="About_Container_Items_Tilte">
+            <h2 class='About_Container_Items_Tilte_Text Text_MarginLeft'>1. Bột nêm Anavira</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents">
+            <p class="About_Container_Items_Content">
+                Tôi từng nghĩ chỉ cần ăn sạch là đủ. Nhưng rồi tôi nhận ra: sạch thôi chưa đủ – còn phải ngon.
+            </p>
+            <p class="About_Container_Items_Content">
+                Người bệnh sẽ không thể ăn mãi rau luộc lạt lẽo, hay cháo nhạt tênh. Người khỏe cũng không ai duy trì thực dưỡng lâu dài nếu bữa ăn nhạt nhẽo như hình phạt.
+            </p>
+            <p class="About_Container_Items_Content">
+                ấm, rau củ, không hương liệu, không bột ngọt. Tôi sấy từng mẻ hành, tỏi, gừng, cà rốt, củ sắn… rồi phối trộn cùng nấm bào ngư, nấm hương, tiêu, thảo mộc – để tạo nên một thứ gia vị vừa sâu vị, vừa lành tính.
+            </p>
+
+            <p class="About_Container_Items_Content"><strong> Bột nêm không chỉ là gia vị. Nó là trái tim của món ăn.</strong></p>
+            <p class="About_Container_Items_Content"><strong> Và với Anavira, nó là nơi tôi gửi gắm sự tỉnh thức – vào từng thìa nêm.</strong></p>
+
+        </div>
+
+        <div class="About_Container_Items_Tilte">
+            <h2 class='About_Container_Items_Tilte_Text Text_MarginLeft'>2. Nước mắm chay Anavira</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents ">
+            <p class="About_Container_Items_Content">
+                Tôi sinh ra trong nền ẩm thực Việt – nơi nước mắm là linh hồn của bàn ăn. Nhưng khi bước vào hành trình thực dưỡng, tôi không tìm được loại nước chấm nào vừa <strong>thơm đậm hậu vị</strong>, vừa <strong>không hóa chất</strong>, lại <strong>không dùng nấm men công nghiệp</strong>. </p>
+            <p class="About_Container_Items_Content">
+                Vậy là tôi tự tay nấu thử. </p>
+            <p class="About_Container_Items_Content">
+                Từng mẻ nước mắm chay Anavira được chưng cất từ <strong>dứa tươi, nấm hương, tiêu hột, gừng, cỏ ngọt...</strong> và nước tương nguyên chất.Không hương liệu. Không chất bảo quản. Không lên men nhân tạo. Chỉ là những nguyên liệu thật, nấu trong thời gian thật – để tạo nên một vị chấm <strong>tròn đầy – dịu mặn – thơm sâu – và lành</strong>.
+            </p>
+
+            <p class="About_Container_Items_Content"><strong>Vì nước mắm không chỉ là nước chấm.</strong></p>
+            <p class="About_Container_Items_Content"><strong>Nó là ký ức, là bản sắc – và là nơi kết nối những người xa quê bằng một vị thân quen.</strong></p>
+
+        </div>
+
+        <div class="About_Container_Items_Tilte">
+            <h2 class='About_Container_Items_Tilte_Text Text_MarginLeft'>3. Cháo dinh dưỡng ăn liền Anavira</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents ">
+            <p class="About_Container_Items_Content">
+                Sau mỗi đợt hóa trị, tôi từng ngồi trước tô cơm mà không nuốt nổi – nhưng vẫn <strong>phải ăn</strong> để đủ sức hồi phục. Xạ trị kéo dài gần tháng, cơ thể đuối dần – và tôi hiểu rằng: <strong>Ăn là để sống tiếp.</strong>
+            </p>
+            <p class="About_Container_Items_Content">
+                Vậy là tôi tự tay nấu thử.
+            </p>
+            <p class="About_Container_Items_Content">
+                Cháo Anavira ra đời cho những lúc yếu nhất đời người. Không chỉ cho người ung thư, mà cho bất kỳ ai đang nằm viện – cần một chén cháo <strong>sạch, đủ chất, dễ tiêu – và thật sự lành.</strong>
+            </p>
+
+            <p class="About_Container_Items_Content">
+                Giữa những phần ăn công nghiệp vô cảm, một chén cháo rau củ, các loại nấm và ngũ cốc sạch sấy thăng hoa – có thể là <strong>sự nâng đỡ âm thầm nhất.</strong>
+            </p>
+
+        </div>
+
+        <div class="About_Container_Items_Tilte">
+            <h2 class='About_Container_Items_Tilte_Text  Text_MarginLeft'>4. Pâté – chả – nhân bánh mì chay độc quyền Anavira</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents ">
+            <p class="About_Container_Items_Content">
+                Tôi từng mất vị giác sau hóa trị. Nhiều ngày liền chỉ nuốt qua loa vài miếng cháo lỏng. Nhưng rồi tôi nghĩ: nếu sống sót, tôi muốn ăn lại một ổ bánh mì – thật sự sạch – thật sự ngon – đúng nghĩa một bữa ăn sáng lành mạnh, không dầu, không bột ngọt, không hàng xáo.
+            </p>
+            <p class="About_Container_Items_Content">
+                Và tôi bắt đầu thử làm… từ những mẻ pate đầu tiên, đến từng đòn chả, từng mẻ bánh sourdough ủ men tự nhiên.
+            </p>
+            <p class="About_Container_Items_Content">
+                Tôi không muốn người bệnh – hay người khỏe – phải chọn giữa “ngon” và “lành”. Anavira tạo ra các nhân bánh mì fusion Đông – Tây, đậm đà nhưng không gắt, đủ no nhưng không ngán, phù hợp khẩu vị Việt mà vẫn tươi mới, độc đáo. </p>
+
+            <p class="About_Container_Items_Content">
+                Vì một bữa sáng đủ đầy, sạch lành, cũng có thể là bước đầu hồi phục.
+            </p>
+
+        </div>
+    </div>
+    <div class="About_Container_Items">
+        <div class="About_Container_Items_Tilte">
+            <div class="About_Container_Items_Tilte_Icon">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
+            </div>
+            <h2 class='About_Container_Items_Tilte_Text'>Chúng tôi tin điều gì?</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents">
+            <p class="About_Container_Items_Content">
+                <span>•</span>
+                <strong>Tỉnh thức</strong> là một lựa chọn sống. Không chấp nhận những gì có sẵn nếu điều đó không giúp bạn khỏe mạnh hơn.
+            </p>
+            <p class="About_Container_Items_Content">
+                <span>•</span>
+                <strong>Ăn uống là con đường hồi sinh</strong>. Không phải mọi căn bệnh đều chữa được, nhưng ai cũng có thể sống tốt hơn qua từng bữa ăn.
+            </p>
+            <p class="About_Container_Items_Content">
+                <span>•</span>
+                <strong>Trở lại với đơn sơ</strong> – là tiến hóa, không phải thoái lui.
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="About_Container_Items">
+        <div class="About_Container_Items_Tilte">
+            <div class="About_Container_Items_Tilte_Icon">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
+            </div>
+            <h2 class='About_Container_Items_Tilte_Text'>Người sáng lập</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents">
+            <p class="About_Container_Items_Content">
+                Tôi là một bệnh nhân ung thư nội mạc tử cung giai đoạn 3A. Tôi đã phẫu thuật, hóa trị, xạ trị. Đã từng đau đến mức tưởng không sống nổi. Nhưng tôi đã sống. Không nhờ thần dược, mà nhờ một hành trình điều chỉnh lối sống và nhận thức.
+            </p>
+            <p class="About_Container_Items_Content">
+                Tôi chọn lối đi không dễ dàng: không bám vào đường – sữa – dầu – đạm động vật – thuốc bổ. Tôi quay về với ngũ cốc, rau củ, men vi sinh, thực phẩm sạch – đúng nghĩa tự nhiên.
+            </p>
+            <p class="About_Container_Items_Content">
+                Tôi không lập Anavira để dạy ai phải sống sao. Tôi chỉ viết – chia sẻ – và đưa ra những gì tôi đã dùng, đã làm, đã đi qua. Nếu bạn thấy mình trong đó, hãy đồng hành. Còn nếu không, tôi vẫn biết ơn bạn vì đã đọc.
+            </p>
+
+        </div>
+
+    </div>
+    <div class="About_Container_Items">
+        <div class="About_Container_Items_Tilte">
+            <div class="About_Container_Items_Tilte_Icon">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
+            </div>
+            <h2 class='About_Container_Items_Tilte_Text'>Tôi và Anavira</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents">
+            <p class="About_Container_Items_Content">
+                Anavira là bài thơ cuối đời, là bản đồ tái sinh, là hành trình thức tỉnh được viết bằng mồ hôi và máu. Tôi không hoàn hảo, nhưng tôi sống thật. Tôi không hứa sẽ làm bạn hài lòng, nhưng tôi sẽ làm mọi thứ bằng trái tim tuyệt đối không thỏa hiệp.
+            </p>
+            <p class="About_Container_Items_Content">
+                Cảm ơn bạn, vì đã bước tới và nhìn thấy Anavira.
+            </p>
+        </div>
+
+    </div>
+    <div class="About_Container_Items">
+        <div class="About_Container_Items_Tilte">
+            <div class="About_Container_Items_Tilte_Icon">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
+            </div>
+            <h2 class='About_Container_Items_Tilte_Text'>Tôi và tiền bạc</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents">
+            <p class="About_Container_Items_Content">
+                Tôi sinh ra trong một gia đình đông con. 19 tuổi, tôi tự mở trung tâm gia sư để đóng học phí – không ai chống lưng, không xin một đồng. Từ đó đến nay, tôi chưa bao giờ gọi vốn hùn làm ăn. Đôi lúc kẹt vốn ngắn hạn, tôi mượn bạn – và luôn trả. Tôi quen tự chịu trách nhiệm.
+            </p>
+            <p class="About_Container_Items_Content">
+                Tôi không lập Anavira để đi gọi vốn. Tôi lập Anavira vì có những người bệnh thật – đang cần một bữa ăn sạch, lành, đúng nghĩa – không dầu, không bột ngọt, không đường công nghiệp, không hứa suông.
+            </p>
+            <p class="About_Container_Items_Content">
+                Mọi quyết định về công thức, nguyên liệu, cách bảo quản… đều ảnh hưởng trực tiếp đến sức khỏe người đang yếu. Tôi không cho phép ai "góp ý thay" lương tâm của mình – chỉ vì đã góp vốn.
+            </p>
+
+
+        </div>
+
+    </div>
+    <div class="About_Container_Items">
+        <div class="About_Container_Items_Tilte">
+            <div class="About_Container_Items_Tilte_Icon">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
+            </div>
+            <h2 class='About_Container_Items_Tilte_Text'>Anavira không nhận vốn đổi lấy quyền can thiệp điều hành.</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents">
+
+            <p class="About_Container_Items_Content">
+                Nếu có ai muốn hỗ trợ, tôi rất biết ơn – nhưng hình thức phải là:
+            </p>
+            <p class="About_Container_Items_Content">
+                <span>•</span>
+                Đặt hàng sớm
+            </p>
+            <p class="About_Container_Items_Content">
+                <span>•</span>
+                Tài trợ nguyên liệu
+            </p>
+            <p class="About_Container_Items_Content">
+                <span>•</span>
+                Đồng hành nghiên cứu không áp lực thương mại hóa gấp
+            </p>
+            <p class="About_Container_Items_Content">
+                <span>•</span>
+                Hoặc đơn giản: mua sản phẩm – chia sẻ nó – và tin vào nó.
+            </p>
+            <p class="About_Container_Items_Content">
+                Tôi sẵn sàng dùng chính thời gian, công sức – và cả tài sản cá nhân để phát triển Anavira.
+                Tôi không tìm người sở hữu thương hiệu này. Tôi chỉ mong tìm được những người hiểu vì sao nó tồn tại.
+            </p>
+            <p class="About_Container_Items_Content">
+                Tôi không lập Anavira để trở thành hình mẫu truyền thông.
+            </p>
+            <p class="About_Container_Items_Content">
+                Tôi chỉ muốn làm một điều gì đó – thật sự sạch – thật sự lành – thật sự sâu sắc – để khi tôi không còn nữa, tôi không tiếc nuối vì đã không sống cho đúng linh hồn mình.
+            </p>
+            <p class="About_Container_Items_Content">
+                Cảm ơn bạn – nếu bạn nhìn thấy chính mình trong Anavira.
+            </p>
+            <p class="About_Container_Items_Content">
+                Chúng ta không phải người tiêu dùng – mà là những ngọn lửa nhỏ, đang tìm nhau giữa thế giới ngột ngạt.
+            </p>
+        </div>
+
+    </div>
+    <div class="About_Container_Items">
+        <div class="About_Container_Items_Tilte">
+            <div class="About_Container_Items_Tilte_Icon">
+                <?php echo file_get_contents(get_template_directory() . '/assets/images/leaf3.svg'); ?>
+            </div>
+            <h2 class='About_Container_Items_Tilte_Text'>Kết</h2>
+        </div>
+
+        <div class="About_Container_Items_Contents">
+            <p class="About_Container_Items_Content">
+                <strong>Anavira – không được sinh ra từ thị trường.</strong>
+            </p>
+            <p class="About_Container_Items_Content">
+                Mà được đánh thức từ hành trình tỉnh thức – mạnh mẽ – và tái sinh.
+            </p>
+
+
+
+        </div>
+
+    </div>
+        <div class="Frontpage_Container_Statement">
+        <p> #Anavira_WFPB </p>
+        <p> #Thức_tỉnh_Mạnh_mẽ_Tái_sinh</p>
+    </div>
+
 </div>
