ce442d4fd96f992fa4eff0d2c3ee5e77f5ef68c0
Đinh Quốc Cường <tr6r20@gmail.com>
2025-08-11 12:30:15 +0700

    UPDATE product page coming soon

--- template-parts/product/part-product-introduction-container.php
+++ template-parts/product/part-product-introduction-container.php
@@ -1,1 +1,19 @@
-INTRODUCTION CONTAINER
+<div class="Introduction_Container">
+    <div class="Introduction_Container_Title">
+        <p>Product
+        <p>
+    </div>
+    <div class="Frontpage_Container_banner">
+
+        <img class="Frontpage_Container_banner_Image" src="<?php echo site_url(); ?>/wp-content/uploads/2025/07/Blog.png" alt="Banner">
+    </div>
+    <div class="Frontpage_Container_Items">
+        <div class="Frontpage_Container_Items_Contents Comingsoon" >
+            <h2 class="Frontpage_Container_Items_Content Comingsoon_Title">
+                COMING SOON
+            </h2>
+
+
+        </div>
+    </div>
+</div>
