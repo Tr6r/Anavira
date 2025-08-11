f64d35c151278f88f2822dd671ed8e929e8408f9
Đinh Quốc Cường <tr6r20@gmail.com>
2025-08-11 12:10:36 +0700

    .

--- page-recipe.php
+++ page-recipe.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Template Name: Page-About-anavira
+ * Template Name: Page-Recipe-anavira
  */
 ?>
 
 
 <?php
-include get_theme_file_path('templates/about/about-page.php');
+include get_theme_file_path('templates/recipe/recipe-page.php');
 ?>
