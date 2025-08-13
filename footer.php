de511cddb75e39e33eec876527986da0d19c9f09
Đinh Quốc Cường <tr6r20@gmail.com>
2025-08-13 17:33:51 +0700

    FIX

--- footer.php
+++ footer.php
@@ -28,26 +28,26 @@
         </div>
     </div>
     <div class="Mobile_Footer_Container">
-            <p>
-                <strong>ANAVIRA WFPB</strong>
-            </p>
-            <p>
-                <em>Thức tỉnh – Mạnh mẽ – Tái sinh</em>
-            </p>
-            <p>
-Website chính thức của Anavira WFPB – Thực phẩm thuần chay toàn phần (Whole-Food Plant-Based)  
-            </p>
-     
-            <p>
-                Email: anavirawfpb@gmail.com
-            </p>
-            <a href="https://www.facebook.com/groups/769453342126267">
-                Facebook: https://www.facebook.com/groups/769453342126267
-            </a>
-            <p>&copy; <?php echo date('Y'); ?> Anavira WFPB. Tất cả các quyền được bảo lưu.</p>
-            <p>
-                Bản quyền website thuộc về Người sáng lập Anavira WFPB.
-            </p>
+        <p>
+            <strong>ANAVIRA WFPB</strong>
+        </p>
+        <p>
+            <em>Thức tỉnh – Mạnh mẽ – Tái sinh</em>
+        </p>
+        <p>
+            Website chính thức của Anavira WFPB – Thực phẩm thuần chay toàn phần (Whole-Food Plant-Based)
+        </p>
+
+        <p>
+            Email: anavirawfpb@gmail.com
+        </p>
+        <a href="https://www.facebook.com/groups/769453342126267">
+            Facebook: https://www.facebook.com/groups/769453342126267
+        </a>
+        <p>&copy; <?php echo date('Y'); ?> Anavira WFPB. Tất cả các quyền được bảo lưu.</p>
+        <p>
+            Bản quyền website thuộc về Người sáng lập Anavira WFPB.
+        </p>
     </div>
 </footer>
 
